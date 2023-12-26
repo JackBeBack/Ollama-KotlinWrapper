426f9c9e56bb70b4f5673576d84657f7c4573614
jack <kloos.jonas@googlemail.com>
2023-12-26 19:13:22 +0100

    updated README

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## How to Use
 Make sure [Ollama](https://ollama.ai/) is Installed and Running
+### Generate a completion
 ```kotlin
 fun main() {
     runBlocking {
@@ -7,6 +8,50 @@ fun main() {
         val ollama = Ollama(model = "wizard-vicuna")
         ollama.generate("What is the weather in Berlin?").collect{
             print(it)
+        }
+    }
+}
+```
+
+### Generate a chat completion
+```kotlin
+fun main() {
+    runBlocking {
+        val ollama = Ollama()
+
+        // A mutable list of Message objects is created to store the chat history.
+        var history = mutableListOf<Message>()
+
+        // An infinite loop is started to continuously accept user input.
+        while (true) {
+            println("User:")
+
+            // The user's input is read from the console.
+            val message = readLine()
+
+            // If the user's input is null or empty, the loop is broken and the program ends.
+            if (message.isNullOrEmpty()) break
+
+            println("Assistant:")
+
+            // The user's message is added to the chat history.
+            history.add(Message(Role.USER, message))
+
+            // The chat method of the Ollama object is called with the chat history and a callback
+            // function.
+            // The callback function updates the chat history with the new history returned by the
+            // chat method.
+            ollama.chat(
+                history,
+                onFinish = {
+                    history = it.toMutableList()
+                    println()
+                }
+            )
+                .collect {
+                    // The messages from the chat are collected and printed to the console.
+                    print(it)
+                }
         }
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,46 @@
 package de.jackBeBack
 
-import de.jackBeBack.data.ChatRequest
 import de.jackBeBack.data.Message
 import de.jackBeBack.data.Role
 import kotlinx.coroutines.runBlocking
-import java.sql.SQLOutput
 
 fun main() {
     runBlocking {
         val ollama = Ollama()
+
+        // A mutable list of Message objects is created to store the chat history.
         var history = mutableListOf<Message>()
-        while (true){
+
+        // An infinite loop is started to continuously accept user input.
+        while (true) {
             println("User:")
+
+            // The user's input is read from the console.
             val message = readLine()
+
+            // If the user's input is null or empty, the loop is broken and the program ends.
             if (message.isNullOrEmpty()) break
+
             println("Assistant:")
+
+            // The user's message is added to the chat history.
             history.add(Message(Role.USER, message))
-            ollama.chat(history, onFinish = {
-                history = it.toMutableList()
-                println()
-            }).collect{
-                print(it)
-            }
+
+            // The chat method of the Ollama object is called with the chat history and a callback
+            // function.
+            // The callback function updates the chat history with the new history returned by the
+            // chat method.
+            ollama.chat(
+                history,
+                onFinish = {
+                    history = it.toMutableList()
+                    println()
+                }
+            )
+                .collect {
+                    // The messages from the chat are collected and printed to the console.
+                    print(it)
+                }
         }
     }
 }
